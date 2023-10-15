af889c9f0fff68be4f7273a79d3a9eb2031c2285
Ritesh Maurya <119353953+riteshmaurya089@users.noreply.github.com>
2023-10-16 02:42:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+let hamMenuIcon = document.getElementById("ham-menu");
+let navBar = document.getElementById("nav-bar");
+let navLinks = navBar.querySelectorAll("li");
+
+hamMenuIcon.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+  hamMenuIcon.classList.toggle("fa-times");
+});
+navLinks.forEach((navLinks) => {
+  navLinks.addEventListener("click", () => {
+    navBar.classList.remove("active");
+    hamMenuIcon.classList.toggle("fa-times");
+  });
+});
+
+
+function openResume() {
+  window.open(
+    'https://drive.google.com/file/d/162TvKgGsksRNXV1t09NAuZ6kw1kic6wU/view?usp=sharing',
+    '_blank'
+  );
+}
+
+function paddingFunction() {
+  let x = doccument.getElementById("home")
+  x.style.padding = "100px"
+}
+
+document.querySelector("form").addEventListener("submit", saveFile );
+
+
+
+function saveFile(e) {
+
+  e.preventDefault()
+
+
+  let Formdata = JSON.parse(localStorage.getItem("portfolioData")) || []
+
+  let name=document.getElementById("fname").value
+  let email=document.getElementById("email").value
+  let text=document.getElementById("subject").value
+  
+  let data = {
+    "name": name,
+    "email": email,
+    "text": text
+  }
+  Formdata.push(data)
+  localStorage.setItem("portfolioData", JSON.stringify(Formdata))
+}
+
+
+
